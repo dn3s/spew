4ba26a8e09c140c3647cb39a76aecdd9843efa8c
dn3s <d@devonsawatzky.ca>
2015-12-17 22:50:43 -0600

    moved the string building out of printpass() into add(). now it only touches the chars being changed.

--- password.c
+++ password.c
@@ -21,31 +21,34 @@ void addDigit(size_t n)
 		fprintf(stderr, "Realloc failed!\n");
 		exit(1);
 	}
-	if(!realloc(pass, passl+1)){ //don't forget the trailing null!
+	if(!realloc(pass, passl+1)){ //add another char for the null terminator!
 		fprintf(stderr, "Realloc failed!\n");
 		exit(1);
 	}
 	pass[passl]=0;
-	for(i=passl-1; i>0; i--) {
+	for(i=passl-1; i>0; i--) { //bump over current digits to the right by n places
 		passn[i]=passn[i-n];
+		pass[i]=pass[i-n];
 	}
-	for(i=0; i<n; i++) {
+	for(i=0; i<n; i++) { //initialize the new digits to zero.
 		passn[i]=0;
+		pass[i]=chars[0];
 	}
 }
 void add(uint_fast32_t n) //Advance the string by n permutations
 {
-	size_t d=passl-1;
+	size_t d=passl; //d is the digit to start with: the last digit. Note the last digit is actually passl-1, but we'll let the decrement operation in the while loop deal with that FOR OPTIMIZATION
 	while(n) {
-		uint_fast32_t carry=(passn[d]+n)/charsl;
-		passn[d]=(passn[d]+n)%charsl;
-		n=carry;
-		if(n && (d==0)) {
-			addDigit(1);
+		d--; //next digit. This is at the start FOR OPTIMIZATION.
+		uint_fast32_t carry=(passn[d]+n)/charsl; //the amount to carry over to the next digit
+		passn[d]=(passn[d]+n)%charsl; //add n to the current digit, mod charsl
+		pass[d]=chars[passn[d]];
+		n=carry; //and now discard the remainder "used up" in this digit by just setting it to carry
+		if((d==0) && n) { //if n is non-zero (there's still more to add) and we've run out of digits, add another digit, and bump indexes accordingly. The order is chosen since d==0 is less frequently true than n is (short-circuit eval gains)
+			addDigit(1); //we could do this in bursts instead of every time, to save on malloc overhead, but adding digits doesn't happen frequently (at least for normal use cases), and the complexities introduced into every cycle to deal with that may even outweigh the benefits once addigin digits (inevitably) becomes more rare further into runtime.
 			n--;
 			d++;
 		}
-		d--;
 	}
 }
 void printpass()
@@ -60,15 +63,18 @@ void printpass()
 	}
 	printf(prefix);
 	for(i=0; i<passl; i++) {
-		pass[i]=chars[passn[i]];
+		//pass[i]=chars[passn[i]];
 	}
 	puts(pass);
 }
 void initpass(uint_fast32_t l)
 {
+	//allocate memory
 	passn=calloc(l,sizeof(char));
 	pass=calloc(l+1,sizeof(char));
+	//initialize values
 	pass[l]=0;
+	pass[0]=chars[0];
 	passl=l;
 }
 void setCharset(char* c)
